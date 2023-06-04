3e71183ace16a445db5c2beb45abde3ee320b0c2
kdrai007 <987kundanrai@gmail.com>
2023-06-04 12:39:27 +0530

    initial commit

---
+++ src/utils/Assets.ts
@@ -0,0 +1,7 @@
+export const myIconsUrl = [
+  'https://www.twitter.com',
+  'https://www.youtube.com',
+  'https://www.instagram.com',
+  'https://www.github.com/kdrai007',
+  'https://www.reddit.com',
+];
